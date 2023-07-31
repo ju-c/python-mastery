c8657315de41b9145aea1610524da7d258c76b78
Julien <julien.cortesi@protonmail.com>
2023-07-31 17:00:53 +0200

    adding exercise 1.6

--- Exercises/pcost.py
+++ Exercises/pcost.py
@@ -12,4 +12,5 @@ def portfolio_cost(filename):
                 print("Reason:", e)
     return total_price
 
-print(portfolio_cost("../Data/portfolio3.dat"))
+if __name__ == '__main__':
+    print(portfolio_cost("../Data/portfolio.dat"))
