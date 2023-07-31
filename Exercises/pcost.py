71902dfe656b7886af27f1ec9b0cfae155d7d6b2
Julien <julien.cortesi@protonmail.com>
2023-07-31 14:32:02 +0200

    Adding exercise 1.4

--- Exercises/pcost.py
+++ Exercises/pcost.py
@@ -1,7 +1,15 @@
-with open("../Data/portfolio.dat", 'r') as f:
-    total_price = 0.0
-    for line in f:
-        line = line.split()
-        share_x_purchase_price = int(line[1]) * float(line[2])
-        total_price = total_price + share_x_purchase_price
-print(total_price)
+def portfolio_cost(filename):
+    with open(filename, 'r') as f:
+        total_price = 0.0
+        for line in f:
+            line = line.split()
+            try:
+                shares = int(line[1])
+                purchase_price = float(line[2])
+                total_price = total_price + (shares *  purchase_price)
+            except ValueError as e:
+                print("Couldn't parse", line)
+                print("Reason:", e)
+    return total_price
+
+print(portfolio_cost("../Data/portfolio3.dat"))
