0301a902fcc58ee62c29dc0d9a97e82c65af5ff8
Julien <julien.cortesi@protonmail.com>
2023-07-31 10:22:37 +0200

    fixing a typo

--- Exercises/pcost.py
+++ Exercises/pcost.py
@@ -2,6 +2,6 @@ with open("../Data/portfolio.dat", 'r') as f:
     total_price = 0.0
     for line in f:
         line = line.split()
-        share_x_pruchase_price = int(line[1]) * float(line[2])
-        total_price = total_price + share_x_pruchase_price
+        share_x_purchase_price = int(line[1]) * float(line[2])
+        total_price = total_price + share_x_purchase_price
 print(total_price)
